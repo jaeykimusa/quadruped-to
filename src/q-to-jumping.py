8d761a0e7364a7ba25d029f3a0f5791ffc29f38d
Jaey Kim <jkim232@ncsu.edu>
2025-04-08 00:24:09 -0400

    [Skeleton] Re-established project env.

---
+++ src/q-to-jumping.py
@@ -0,0 +1,12 @@
+# Quadrupedal trajectory optimization for jumping
+# Author: jaeykimusa
+
+
+from collections import namedtuple
+import casadi as ca
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
+from IPython.display import HTML, display
+
+

c0f03a8fe46cfb2a2117d408cfe9bae3ab4decaf
Jaey Kim <jkim232@ncsu.edu>
2025-05-14 16:47:50 -0400

    [Update] Up to date.

---
+++ src/kinematics/robot.py
@@ -0,0 +1,9 @@
+
+# Physical parameters
+m = 32.7  # kg boston dynamic's spot
+L = 1.1  # body length (m)
+h = 0.1 # body heigh (m) - ideally 0.2
+I_z = 1 / 12 * m * (L**2 + h**2)  # moment of inertia for rectangular body around the z-axis 
+
+leg_length = 0.3  # m - ideally 0.3-0.35
+
